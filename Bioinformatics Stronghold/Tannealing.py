9c1aad443a5c79afeeb72e4ac8c01f7d30175a51
Ana de la Fuente González <125312689+anadlfuente@users.noreply.github.com>
2023-03-20 17:42:22 +0100

    Update Tannealing.py

--- Bioinformatics Stronghold/Tannealing.py
+++ Bioinformatics Stronghold/Tannealing.py
@@ -1,11 +1,10 @@
 s=input('Enter the oligo sequence:')
 dic= {'AT':0,'GC':0 }
 s=list(s)
-print(s)
 for i in s:
     if i in ['A', 'T']:
         dic['AT']=dic['AT']+1
     else:
         dic['GC']=dic['GC']+1
-Tan= d['GC']*4+d['AT]*2
-print('The temperature of annealing is:',Tan)
+Tan=dic['GC']*4+dic['AT']*2
+print('The temperatur of annealing is:',Tan,'ºC')
